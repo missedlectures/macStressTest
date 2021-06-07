d18676a99e2de54d6e9b29b100f37d7af80062bd
zkascak <zkascak@gmail.com>
2021-06-07 17:06:49 -0400

    Fixes #1

--- macStressTest.sh
+++ macStressTest.sh
@@ -8,21 +8,33 @@
 #
 # Conducts a basic stress test on a system running macOS.
 #
-# 23 May 2021
-# V0.1
+# 2 June 2021
+# V0.2
 # Zachary Kascak @zkascak
 
 # Greet User
 echo "\nMissed Lectures Media\n"
 echo "Mac Stress Test"
-echo "V 0.1\n\n"
-echo "THIS TEST WILL TAKE 15 MINUTES TO COMPLETE\n\n"
+echo "V 0.2\n\n"
+
+# Get system information
+echo "System Info:"
+sysctl -n kern.hostname
+cpu=$(sysctl -n machdep.cpu.brand_string)
+physCores=$(sysctl -n machdep.cpu.core_count)
+mem=$(sysctl -n hw.memsize)
+memout=$(( $mem/1024/1024/1024 ))
+echo "CPU:" $cpu
+echo "Physical Cores:" $physCores
+echo "RAM(GB):" $memout
+echo "\n\nTHIS TEST WILL TAKE 15 MINUTES TO COMPLETE\n\n"
+
+testCycle=90
 
 # Start 15 minute yes test
 echo "yes test starting:"
 echo -n -e "0%.."
 
-testCycle=90
 yes > /dev/null & yes > /dev/null & yes > /dev/null & yes > /dev/null & yes > /dev/null & yes > /dev/null & yes > /dev/null & yes > /dev/null & sleep $testCycle
 
 counter=1
